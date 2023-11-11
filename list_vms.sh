0c25c543223b1fc9764b3a3b7f0571af14ba5f8b
john <martin.john@outlook.com>
2023-11-11 04:58:16 +0000

    troubleshooting

--- list_vms.sh
+++ list_vms.sh
@@ -6,5 +6,6 @@ ESXI_USER="root"
 SSH_KEY="/home/john/.ssh/id_ecdsa"
 
 # Command to list all VMs on the ESXi server
-ssh -i ${SSH_KEY} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${ESXI_USER}@${ESXI_HOST} "esxcli vm process list"
+# ssh -i ${SSH_KEY} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${ESXI_USER}@${ESXI_HOST} "esxcli vm process list"
 
+ssh ${ESXI_USER}@${ESXI_HOST} "esxcli vm process list"
