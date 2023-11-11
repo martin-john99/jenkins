bc91cfe6cf736ca9ad2e9d277173e314d737c50c
john <martin.john@outlook.com>
2023-11-11 04:20:34 +0000

    added ssh key

--- list_vms.sh
+++ list_vms.sh
@@ -3,7 +3,8 @@
 # ESXi server details
 ESXI_HOST="r730xd-esxi"
 ESXI_USER="root"
+SSH_KEY="/home/john/.ssh/id_rsa
 
 # Command to list all VMs on the ESXi server
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${ESXI_USER}@${ESXI_HOST} "esxcli vm process list"
+ssh -i ${SSH_KEY} -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ${ESXI_USER}@${ESXI_HOST} "esxcli vm process list"
 
